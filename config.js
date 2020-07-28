fec26c5daeea9ea2e92f9f6b4e7a51d0d35f3a2b
Bojan Gabric <bojan@bojangabric.com>
2020-07-28 12:15:01 +0200

    Add site config

---
+++ config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  url: 'https://bojangabric.com',
+  pathPrefix: '/',
+  title: 'Bojan Gabric',
+  description: 'Bojan Gabric is a web application consultant that helps early-stage startups kick-off their MVP',
+  googleAnalyticsId: '',
+  author: {
+    name: 'Bojan Gabric',
+    icon: '/icon.png',
+    photo: '/photo.png',
+    bio: '',
+    contacts: {
+      email: 'bojan@bojangabric.com',
+      facebook: 'bojangabric',
+      telegram: '',
+      twitter: 'bojangabric',
+      github: 'bojangabric',
+      rss: '',
+      linkedin: 'bojangabric',
+      instagram: '',
+      codepen: 'bojangabric',
+      youtube: '',
+      angel: 'bojangabric',
+      dribbble: 'bojangabric'
+    }
+  }
+};
