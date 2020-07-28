41efc1b6f41137992a8cc0c477de9ddfb4b2764b
Bojan Gabric <bojan@bojangabric.com>
2020-07-28 12:17:41 +0200

    Update SEO component

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,88 +1,73 @@
-/**
- * SEO component that queries for data with
- *  Gatsby's useStaticQuery React hook
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
-import React from "react"
-import PropTypes from "prop-types"
-import { Helmet } from "react-helmet"
-import { useStaticQuery, graphql } from "gatsby"
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import useSiteMetadata from '../hooks/use-site-metadata';
 
 function SEO({ description, lang, meta, title }) {
-  const { site } = useStaticQuery(
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-            description
-            author
-          }
-        }
-      }
-    `
-  )
-
-  const metaDescription = description || site.siteMetadata.description
+  const siteMetadata = useSiteMetadata();
+  const metaTitle = `${title} | ${siteMetadata.title}`;
+  const metaDescription = description || siteMetadata.description;
 
   return (
     <Helmet
       htmlAttributes={{
-        lang,
+        lang
       }}
-      title={title}
-      titleTemplate={`%s | ${site.siteMetadata.title}`}
+      title={metaTitle}
       meta={[
         {
           name: `description`,
-          content: metaDescription,
+          content: metaDescription
         },
         {
           property: `og:title`,
-          content: title,
+          content: metaTitle
+        },
+        {
+          property: `og:image`,
+          content: `${siteMetadata.url}/photo.png`
         },
         {
           property: `og:description`,
-          content: metaDescription,
+          content: metaDescription
         },
         {
           property: `og:type`,
-          content: `website`,
+          content: `website`
         },
         {
           name: `twitter:card`,
-          content: `summary`,
+          content: `summary`
+        },
+        {
+          name: `twitter:image`,
+          content: `${siteMetadata.url}/photo.png`
+        },
+        {
+          name: `twitter:site`,
+          content: `@bojangabric`
         },
         {
           name: `twitter:creator`,
-          content: site.siteMetadata.author,
+          content: `@bojangabric`
         },
         {
           name: `twitter:title`,
-          content: title,
+          content: metaTitle
         },
         {
           name: `twitter:description`,
-          content: metaDescription,
-        },
+          content: metaDescription
+        }
       ].concat(meta)}
-    />
-  )
+    ></Helmet>
+  );
 }
 
 SEO.defaultProps = {
   lang: `en`,
   meta: [],
   description: ``,
-}
+  title: ``
+};
 
-SEO.propTypes = {
-  description: PropTypes.string,
-  lang: PropTypes.string,
-  meta: PropTypes.arrayOf(PropTypes.object),
-  title: PropTypes.string.isRequired,
-}
-
-export default SEO
+export default SEO;
