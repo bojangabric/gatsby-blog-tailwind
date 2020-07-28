adb43f94f96445a95121c2d1ca8101ad7e477242
Bojan Gabric <bojan@bojangabric.com>
2020-07-28 12:16:33 +0200

    Add hook for site metadata

---
+++ src/hooks/use-site-metadata.js
@@ -0,0 +1,40 @@
+import { useStaticQuery, graphql } from 'gatsby';
+
+const useSiteMetadata = () => {
+  const { site } = useStaticQuery(
+    graphql`
+      query SiteMetaData {
+        site {
+          siteMetadata {
+            author {
+              name
+              bio
+              photo
+              contacts {
+                facebook
+                linkedin
+                github
+                twitter
+                telegram
+                instagram
+                dribbble
+                rss
+                angel
+                codepen
+                youtube
+                email
+              }
+            }
+            url
+            title
+            description
+          }
+        }
+      }
+    `
+  );
+
+  return site.siteMetadata;
+};
+
+export default useSiteMetadata;
